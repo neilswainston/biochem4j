dad1478b7e32b2e2ddabf8bb25dc9496f4afc318
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-02-10 13:52:58 +0000

    Moved biochem4j client code to prevent circular dependencies.

--- sbcdb/query/test/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-SYNBIOCHEM-DB (c) University of Manchester 2015
-
-SYNBIOCHEM-DB is licensed under the MIT License.
-
-To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
-
-@author:  neilswainston
-'''
