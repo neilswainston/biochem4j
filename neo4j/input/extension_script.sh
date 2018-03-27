806eac6a2f69d0d6418a234ee43716423257e22c
Neil Swainston <neil.swainston@manchester.ac.uk>
2018-03-27 14:17:12 +0100

    Updated extension script.

--- neo4j/input/extension_script.sh
+++ neo4j/input/extension_script.sh
@@ -1,7 +1,17 @@
-#!/usr/bin/env bash
-
 rm -rf /var/lib/neo4j/data/databases/graph.db
-/var/lib/neo4j/bin/neo4j-admin import --nodes `ls -d /input/nodes/* | paste -sd "," -` --relationships `ls -d /input/rels/* | paste -sd "," -`
+
+nodes=`ls -d /input/nodes/*`
+rels=`ls -d /input/rels/*`
+nodes_str=`echo $nodes | sed "s/ / --nodes /g"`
+rels_str=`echo $rels| sed "s/ / --relationships /g"`
+
+/var/lib/neo4j/bin/neo4j-admin \
+	import \
+	--nodes $nodes_str \
+	--relationships $rels_str \
+	--delimiter ";" \
+	--array-delimiter "|" \
+	--multiline-fields true
 	
 sed -i 's/#dbms.read_only=false/dbms.read_only=true/g' conf/neo4j.conf
 sed -i 's/#dbms.connector.http.address/dbms.connector.http.address/g' conf/neo4j.conf
