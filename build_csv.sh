97c0c8b3be892658606bc6e1566497fc3503ee1d
Neil Swainston <neil.swainston@manchester.ac.uk>
2018-03-29 23:56:35 +0100

    Added escape char.

--- build_csv.sh
+++ build_csv.sh
@@ -3,4 +3,4 @@
 DIR=$(cd "$(dirname "$0")"; pwd)
 
 docker build -t biochem4j-build .
-docker run -d -v $DIR/neo4j:/biochem4j/neo4j biochem4j-build neo4j/input | True
+docker run -d -v $DIR/neo4j:/biochem4j/neo4j biochem4j-build neo4j/input \| True
