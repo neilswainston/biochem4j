2acbd04e401a11b10e1e70f92ed4212477cf2200
neilswainston <neil.swainston@manchester.ac.uk>
2018-04-12 11:27:53 +0100

    Reconfigured Docker run script.

--- start_db.sh
+++ start_db.sh
@@ -6,10 +6,9 @@ docker run \
 --detach \
 --user neo4j \
 --publish=80:7474 \
---publish=443:7473 \
 --publish=7687:7687 \
 --volume=$DIR/neo4j/input:/input \
--e NEO4J_AUTH=none \
--e NEO4J_dbms_read__only=true \
--e EXTENSION_SCRIPT=/input/extension_script.sh \
+--env=NEO4J_AUTH=neo4j/biochem4j \
+--env=NEO4J_dbms_read__only=true \
+--env=EXTENSION_SCRIPT=/input/extension_script.sh \
 neo4j
