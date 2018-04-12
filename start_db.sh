b6a3f9ed487a73ba2742ea1dba7412e3f9931178
neilswainston <neil.swainston@manchester.ac.uk>
2018-04-12 10:14:56 +0100

    Updated docker run script.

--- start_db.sh
+++ start_db.sh
@@ -6,8 +6,10 @@ docker run \
 --detach \
 --user neo4j \
 --publish=80:7474 \
+--publish=443:7473 \
 --publish=7687:7687 \
 --volume=$DIR/neo4j/input:/input \
 -e NEO4J_AUTH=none \
+-e NEO4J_dbms_read__only=true \
 -e EXTENSION_SCRIPT=/input/extension_script.sh \
 neo4j
